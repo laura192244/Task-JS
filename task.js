0c96044d6aa557457bfbf0e760032d8e7cae83ab
Laura <misakyanlaura@gmail.com>
2026-09-06 22:13:42 +0400

    add task.js

---
+++ task.js
@@ -0,0 +1,63 @@
+let arr=[];
+let i=0
+let count=0;
+
+function task(){    
+if(count<=41){
+let a=(Math.round(Math.random()*41)+1);
+
+if(!arr.includes(a)){
+arr.push(a);
+ console.log(a);
+ count++;
+}else {task();} 
+} 
+else{ console.log(-1);}  
+}
+
+task();
+task();
+task();
+task();
+task();
+task();
+task();
+task();
+task();
+task();
+task();
+task();
+task();
+task();
+task();
+task();
+task();
+task();
+task();
+task();
+task();
+task();
+task();
+task();
+task();
+task();
+task();
+task();
+task();
+task();
+task();
+task();
+task();
+task();
+task();
+task();
+task();
+task();
+task();
+task();
+task();
+task();
+task();
+task();
+task();
+task();
